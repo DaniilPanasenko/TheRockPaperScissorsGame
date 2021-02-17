fbabdff6009a04925028e730fe76492562687820
DaniilPanasenko <daniil.panasenko@nure.ua>
2021-02-17 22:01:38 +0200

    initial commit

---
+++ TheRockPaperScissorsGame.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheRockPaperScissorsGame.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
